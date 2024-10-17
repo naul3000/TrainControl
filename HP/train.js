0b60c18b5b0d0aa3159b9c635f8788308872dddc
naul3000 <naul@gmx.de>
2024-10-17 11:21:14 +0200

    first commit

---
+++ HP/train.js
@@ -0,0 +1,15 @@
+// DOM elements
+const btnTrain1 = document.getElementById("imgTrain1");
+const btnTrain2 = document.getElementById("imgTrain2");
+const spdTrain1 = document.getElementById("spdTrain1");
+const spdTrain2 = document.getElementById("spdTrain2");
+const stpTrain1 = document.getElementById("stpTrain1");
+const stpTrain2 = document.getElementById("stpTrain2");
+const chkSwitch1 = document.getElementById("chkSwitch1");
+const chkSwitch2 = document.getElementById("chkSwitch2");
+const chkSwitch3 = document.getElementById("chkSwitch3");
+const chkSwitch4 = document.getElementById("chkSwitch4");
+
+
+// BLE devices
+var trainName = ["Train1", "Train2"];
